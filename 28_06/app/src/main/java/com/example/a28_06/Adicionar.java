beb26d5c4ef9ffb0c5a636104f795d38a23189b5
marybrachini <marybrachini@gmail.com>
2024-07-05 10:30:37 -0300

    05/07 v2

---
+++ 28_06/app/src/main/java/com/example/a28_06/Adicionar.java
@@ -0,0 +1,12 @@
+package com.example.a28_06;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Adicionar extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adicionar);
+    }
+}
---
+++ 28_06/app/src/main/java/com/example/a28_06/EdicaoInfos.java
@@ -0,0 +1,12 @@
+package com.example.a28_06;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class EdicaoInfos extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edicao);
+    }
+}
