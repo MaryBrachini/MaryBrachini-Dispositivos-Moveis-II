c14d9f55780e396b74a00cc1289bba0117703800
marybrachini <marybrachini@gmail.com>
2024-07-05 10:41:04 -0300

    05/70 v3

--- 19_04/app/src/main/java/com/example/a19_04/MainActivity.java
+++ 19_04/app/src/main/java/com/example/a19_04/MainActivity.java
@@ -2,23 +2,24 @@ package com.example.a19_04;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends Activity {
 
     private String[] disciplinas = {
-        "Disciplina 1", "Disciplina 2", "Disciplina 3",
-        "Disciplina 4", "Disciplina 5", "Disciplina 6",
-        "Disciplina 7"};
+            "Disciplina 1", "Disciplina 2", "Disciplina 3",
+            "Disciplina 4", "Disciplina 5", "Disciplina 6",
+            "Disciplina 7"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-     /*   ArrayAdapter<String> listaDisciplinas = new ArrayAdapter<>(
+        ArrayAdapter<String> listaDisciplinas = new ArrayAdapter<>(
                 this, android.R.layout.simple_list_item_1, disciplinas
         );
 
-        setListAdapter(listaDisciplinas);*/
+        setListAdapter(listaDisciplinas);
     }
 }
