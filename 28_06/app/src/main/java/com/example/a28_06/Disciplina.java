9f7622214c364890783c133747b56a5423024e88
marybrachini <marybrachini@gmail.com>
2024-07-05 11:59:40 -0300

    05/07 v5

---
+++ 28_06/app/src/main/java/com/example/a28_06/Disciplina.java
@@ -0,0 +1,11 @@
+package com.example.a28_06;
+
+import java.io.Serializable;
+
+public class Disciplina implements Serializable {
+
+    // private static final long serialVersionUID = 1633833011084400384L;
+    int id;
+    String nome;
+    String dia;
+}
--- 28_06/app/src/main/java/com/example/a28_06/MainActivity.java
+++ 28_06/app/src/main/java/com/example/a28_06/MainActivity.java
@@ -1,25 +1,35 @@
 package com.example.a28_06;
 
+
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends Activity {
-    String[] Disciplinas = {
-            "Disciplina 1", "Disciplina 2", "Disciplina 3",
-            "Disciplina 4", "Disciplina 5", "Disciplina 6",
-            "Disciplina 7",
-    };
+
+
+
+    private BancoDados bd;
+    private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+    private DisciplinaAdapter adaptadorDisciplina;
+    public static final int REQUEST_EDICAO = 0;
+    public static final int REQUEST_SALVOU = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Botao de Abris as Disciplinas
         abreAdd t1 = new abreAdd();
         t1.setSaida(this);
 
@@ -35,51 +45,111 @@ public class MainActivity extends Activity {
         bt2 = (Button)findViewById(R.id.btnMeses);
         bt2.setOnClickListener(t2);
 
-        //Mostrar a Lista de disciplinas
-        ArrayAdapter<String> arrayDisciplinas = new ArrayAdapter<>(
-                this, android.R.layout.simple_list_item_1, Disciplinas
-        );
-
-        ListView listaDisciplina = (ListView)findViewById(R.id.lista);
-
-        listaDisciplina.setAdapter(arrayDisciplinas);
+        bd = new BancoDados(this);
+        lerDados();
     }
 
-}
+    class abreAdd implements View.OnClickListener {
 
-//classe para abrir a interface de Adicionar Disciplinas
-class abreAdd implements View.OnClickListener {
+        private MainActivity saida;
 
-    private MainActivity saida;
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(saida, Adiciona.class);
+            saida.startActivityForResult(i,0);
+        }
+
+        public void setSaida(MainActivity p) {
+            saida = p;
+        }
+    }
+
+    //classe para abrir a interface de ver Meses
+    class abreMeses implements View.OnClickListener {
+
+        private MainActivity saida;
+
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(saida, Meses.class);
+            saida.startActivity(i);
+        }
+
+        public void setSaida(MainActivity p2) {
+            saida = p2;
+        }
+    }
+
+    public void lerDados() {
+        bd.abrir();
+        disciplinas.clear();
+
+        Cursor cursor = bd.retornaTodosEventos();
+        if (cursor.moveToFirst()) {
+            do {
+                Disciplina d = new Disciplina();
+                d.id = cursor.getInt(cursor.getColumnIndex(BancoDados.KEY_ID));
+                d.nome = cursor.getString(cursor.getColumnIndex(BancoDados.KEY_NOME));
+                d.dia = cursor.getString(cursor.getColumnIndex(BancoDados.KEY_DIA));
+                disciplinas.add(d);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+
+        bd.fechar();
+
+        if (adaptadorDisciplina == null) {
+            adaptadorDisciplina = new DisciplinaAdapter(this, disciplinas) {
+                @Override
+                public void edita(Disciplina disciplina) {
+                    Intent intent = new Intent(getApplicationContext(), Adiciona.class);
+                    intent.putExtra("disciplina", disciplina);
+                    startActivityForResult(intent, REQUEST_EDICAO);
+                }
+
+                @Override
+                public void deleta(Disciplina disciplina) {
+                    bd.abrir();
+                    bd.apagaEvento(disciplina.id);
+                    bd.fechar();
+                    lerDados();
+                }
+            };
+
+            ListView lista = (ListView)findViewById(R.id.lista);
+            lista.setAdapter(adaptadorDisciplina);
+        } else {
+            adaptadorDisciplina.novosDados(disciplinas);
+            adaptadorDisciplina.notifyDataSetChanged();
+        }
+    }
 
     @Override
-    public void onClick(View v) {
-        Intent i = new Intent(saida, Adiciona.class);
-        saida.startActivity(i);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.item_disciplina, menu);
+        return true;
     }
-
-    public void setSaida(MainActivity p) {
-        saida = p;
-    }
-}
-
-//classe para abrir a interface de ver Meses
-class abreMeses implements View.OnClickListener {
-
-
-
-    private MainActivity saida;
 
     @Override
-    public void onClick(View v) {
-        Intent i = new Intent(saida, Meses.class);
-        saida.startActivity(i);
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+
+        if (item.getItemId() == R.id.menu_add) {
+            Intent intent = new Intent (this, Adiciona.class);
+            startActivityForResult(intent, REQUEST_EDICAO);
+            return true;
+        } else {
+            return super.onMenuItemSelected(featureId, item);
+        }
     }
 
-    public void setSaida(MainActivity p2) {
-        saida = p2;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_EDICAO) {
+            if (resultCode == REQUEST_SALVOU) {
+                lerDados();
+            }
+        }
     }
+
+
 }
-
-
-
